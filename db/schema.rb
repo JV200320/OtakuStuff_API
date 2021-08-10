1777c6a769e65bf2d86e721c737c920bad6abcb0
João Victor de Souza Lucena <joaovictordev2003@gmail.com>
2021-08-10 12:44:08 -0300

    Relationship table created, gotta work on checking if there's an existing following relationship already

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_02_002015) do
+ActiveRecord::Schema.define(version: 2021_08_10_134114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
