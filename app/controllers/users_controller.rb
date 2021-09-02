6e65fbf9a49282e363ed54347bb3ddf0d6acb773
João Victor de Souza Lucena <joaovictordev2003@gmail.com>
2021-09-02 10:44:04 -0300

    Fixing user json rendering

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   def search
     @users = User.all.where("nickname ILIKE '%#{params['search']}%'")
+    render 'users/search.json.jbuilder'
   end
 
 end
